ff959f039f2532f33e07032f3a11cf254c48cff7
赖玖湖 <jiuhulai@digitalgd.com.cn>
2023-03-02 08:49:48 +0800

    leetcode: 0502

---
+++ leetcode/2023/3/0502.js
@@ -0,0 +1,11 @@
+const printBin = function(num) {
+    let sb = '0.';
+    while (sb.length <= 32 && num !== 0) {
+        num *= 2;
+        const digit = Math.floor(num);
+        sb += digit;
+        num -= digit;
+    }
+    return sb.length <= 32 ? sb : 'ERROR';
+};
+
